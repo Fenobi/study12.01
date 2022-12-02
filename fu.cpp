5404038e720287ad6fb0a9a5dcc1d4ca091d6707
Fenobi <13667739122@qq.com>
2022-12-02 17:00:42 +0800

    添加 .gitattributes、.gitignore 和 LICENSE.txt。

---
+++ fu.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <list> 
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void test1()
+{
+	list<int> ls(10, 4);
+	ls.push_front(10);
+	ls.push_front(20);
+	ls.push_front(30);
+	ls.push_front(40);
+	//ls.erase(ls.begin(),ls.end());
+	ls.erase(find(ls.begin(), ls.end(), 30));
+	list<int>::iterator it = ls.begin();
+
+	for (auto e : ls)
+	{
+		cout << e << ' ';
+	}
+}
+
+void test2()
+{
+	list<int> ls;
+	ls.push_front(50);
+	ls.push_front(20);
+	ls.push_front(222);
+	ls.push_front(240);
+	ls.push_front(20);
+	ls.push_front(30);
+	ls.push_front(40);
+	//ls.remove(20);
+	ls.sort();
+	ls.unique();
+	for (auto e : ls)
+	{
+		cout << e << ' ';
+	}
+	cout << endl;
+}
+
+int main()
+{
+	//test1();
+	test2();
+	return 0;
+}
